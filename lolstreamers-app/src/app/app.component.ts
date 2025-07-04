37bc9799e74333808a923c9c27ad7cf4aae94163
Christian Scholz <c.scholz@c-s-media.net>
2025-07-04 10:31:03 +0200

    Add FooterComponent to the app structure
    Introduced a new FooterComponent with accompanying HTML, CSS, and tests
    to display key information and navigation links in the app footer.
    Integrated the component into the main AppComponent to ensure it is
    rendered across all pages.

--- lolstreamers-app/src/app/app.component.ts
+++ lolstreamers-app/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {NavigationComponent} from "./navigation/navigation.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, NavigationComponent],
+  imports: [RouterModule, NavigationComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
