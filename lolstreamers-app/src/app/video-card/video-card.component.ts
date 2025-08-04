261b76df4b60a19dc856d7ca4ba3ca17c4c7cc16
Christian Scholz <c.scholz@c-s-media.net>
2025-08-04 13:42:55 +0200

    Add edit link for videos in `VideoCardComponent`
    - Integrated `AuthService` to conditionally display the edit link for logged-in users.
    - Used `RouterModule` for navigation to the edit page.
    - Updated template to include the edit link.

--- lolstreamers-app/src/app/video-card/video-card.component.ts
+++ lolstreamers-app/src/app/video-card/video-card.component.ts
@@ -1,14 +1,19 @@
 import { Component, Input } from '@angular/core';
 import {Video} from "../video";
 import {CommonModule} from "@angular/common";
+import {AuthService} from "../auth.service";
+import {RouterModule} from "@angular/router";
 
 @Component({
   selector: 'app-video-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './video-card.component.html',
   styleUrl: './video-card.component.css'
 })
 export class VideoCardComponent {
  @Input() video!: Video;
+
+  constructor(public authService: AuthService) {
+  }
 }
